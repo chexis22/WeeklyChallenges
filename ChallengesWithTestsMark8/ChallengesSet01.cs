c6e4192a68965103089a9dfd6af90ab1229da307
Alexis Diaz <chexisss@gmail.com>
2020-01-27 13:17:54 -0600

    Set1Done

--- ChallengesWithTestsMark8/ChallengesSet01.cs
+++ ChallengesWithTestsMark8/ChallengesSet01.cs
@@ -6,37 +6,58 @@ namespace ChallengesWithTestsMark8
     {
         public bool AreTwoNumbersTheSame(int num1, int num2)
         {
-            throw new NotImplementedException();
+            if (num1 == num2)
+            {
+                return true;
+            }                   // compare two numbers, return true or false
+
+            else
+            {
+                return false;
+            }
         }
 
         public double Subtract(double minuend, double subtrahend)
         {
-            throw new NotImplementedException();
+            return minuend - subtrahend; // return a subtraction method
         }
 
         public int Add(int number1, int number2)
         {
-            throw new NotImplementedException();
+            return number1 + number2; // return an addition method
         }
 
         public int GetSmallestNumber(int number1, int number2)
         {
-            throw new NotImplementedException();
+            if (number1 < number2)      // return the smallest of the two
+                return number1;
+            else
+                return number2;
         }
 
         public long Multiply(long factor1, long factor2)
         {
-            throw new NotImplementedException();
+            return factor1 * factor2;
         }
 
         public string GetGreeting(string nameOfPerson)
         {
-            throw new NotImplementedException();
+            if (nameOfPerson != "")
+            {
+                return "Hello, " + nameOfPerson + "!";
+            }
+            else
+            {
+                return "Hello!";
+            }
+          
+            
+
         }
 
         public string GetHey()
         {
-            throw new NotImplementedException();
+            return "HEY!"; // return a HEY!
         }
     }
 }
